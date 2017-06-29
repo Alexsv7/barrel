86537217ea588ad47e1006e3a8a48b7921a4f5fc
alexsv7 <alexsv7@ukr.net>
2017-06-29 17:53:03 +0300

    index checking improved

--- barrel.py
+++ barrel.py
@@ -1,43 +1,47 @@
 import os
-from glob import glob
 import re
 import errno
+from glob import glob
 from importStringFormatter import cutimportString
 from ClassModel import ClassModel
 
 
 def get_subdirectories(testModule):
-    filename = create_index_file(testModule)
+    # if checkIfClassExistInDir(testModule):
+        testModule = os.path.normpath(testModule)
+        print testModule
+        filename = create_index_file(os.path.normpath(testModule))
+        with open(filename, "w+") as f:
+            directory_ts_contant = glob(testModule + '/*.ts')
 
-    with open(filename, "w+") as f:
-        # print directory_ts_contant
-        directory_ts_contant = glob(testModule+'/*.ts')
+            for fileName in directory_ts_contant:
+                with open(fileName, 'r')as file:
+                    a = file.read()
 
-        for fileName in directory_ts_contant:
-            with open (fileName, 'r')as file:
-                a = file.read()
-                # className =  re.search('export class[\s]([\s]+)', a)
+                    className = re.search('class\s(.*?)\s', a) or re.search('interface\s(.*?)\s', a)
 
-                className =  re.search('class\s(.*?)\s', a) or re.search('interface\s(.*?)\s', a)
-                # print className\s
+                    if className:
+                        fileName = cutimportString(fileName, testModule)
+                        classObject = ClassModel(className.group(1), fileName)
+                        importString = classObject.genrateImport()
+                        f.write(importString)
+                        f.write('\n')
 
-                if className:
-                    fileName = cutimportString(fileName, testModule)
-                    classObject = ClassModel(className.group(1),fileName)
-                    importString =  classObject.genrateImport()
-                    f.write(importString)
+
+            directory_folder_contant = glob(testModule+'/*/')
+
+            for file_path in directory_folder_contant:
+                # if glob(file_path+'index.ts'):
+                #     print glob(file_path+'index.ts')
+                    formated_file_path = cutimportString(file_path,testModule)
+
+                    #### check if directory contain index file and just then create parenetal index and make export
+
+                    classObject = ClassModel('', formated_file_path)
+                    importDirectoryString = classObject.genrateImportForDirectory()
+                    f.write(importDirectoryString)
                     f.write('\n')
-
-        directory_folder_contant = glob(testModule+'/*/')
-
-        for fileName in directory_folder_contant:
-            # fileName = cutimportString(fileName,testModule)
-
-            classObject = ClassModel('', fileName)
-            importDirectoryString = classObject.genrateImportForDirectory()
-            f.write(importDirectoryString)
-            f.write('\n')
-            get_subdirectories(fileName)
+                    get_subdirectories(file_path)
 
 
 def create_index_file(baseDir):
@@ -51,5 +55,21 @@ def create_index_file(baseDir):
                 raise
     return filename
 
+def checkIfClassExistInDir(dir_path):
+    isExist = False
+
+    directory_ts_contant = glob(dir_path + '/*.ts')
+    for file_path in directory_ts_contant:
+        with open(file_path, 'r')as file:
+            a = file.read()
+            className = re.search('class\s(.*?)\s', a) or re.search('interface\s(.*?)\s', a)
+            if className:
+                isExist = True
+                return True
+
+    directory_folder_contant = glob(dir_path + '/*/')
+    for file_path in directory_folder_contant:
+        if not isExist:
+            checkIfClassExistInDir(file_path)
 
 get_subdirectories('/Users/alex/tooling-spa/frontend/src/main/tooling-spa/src/app/shared')
--- importStringFormatter.py
+++ importStringFormatter.py
@@ -3,13 +3,11 @@ import os
 
 def cutimportString(importString,testModule):
     nestingCount = importString.count('/') - testModule.count('/')
-    # print nestingCount
-    # print importString.rsplit('/',nestingCount)
-    # print os.path.split(testModule)[1]
+
     rel = os.path.relpath(importString, testModule)
 
-    print importString
-    print testModule
-    print './'+rel
+    # print importString
+    # print testModule
+    # print './'+rel
 
     return './'+rel
